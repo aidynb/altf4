a441669df540d7c3d5ddc8813771bd02ddb429c6
aidyn <aidyn.baigarinov@gmail.com>
2021-10-04 08:57:48 +0600

    add depth comment

--- config/constants.go
+++ config/constants.go
@@ -1,6 +1,7 @@
 package config
 
 const SYMBOL = "btcusdt"
+// 20 вместо 15, потому что в документации есть только 3 варианта: 5, 10, 20. Если правильно понял задачу, то количество ордеров в стакане относится к этому (Кол-во ордеров в каждом стакане - 15)
 const DEPTH = "20"
 const UPDATE_SPEED = "1000ms"
 const BASE_URL = "wss://stream.binance.com:9443/ws/"
